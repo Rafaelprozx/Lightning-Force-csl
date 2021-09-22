3a4aeea102ee061d19f1832e8b092eac736b7051
Rafaelprozx <42326761+Rafaelprozx@users.noreply.github.com>
2021-09-22 16:38:14 -0400

    actualizacion 1

--- core/src/csl/espacio/clases/Colisionable.java
+++ core/src/csl/espacio/clases/Colisionable.java
@@ -7,11 +7,11 @@ public interface Colisionable {
 	public Rectangle col();
 	 public static class statics{
 		 public static boolean In_cam_range(Colisionable c,Camera cam){
-			 return !(c.col().x < cam.position.x-320 || c.col().y < cam.position.y-200 || c.col().x > (cam.position.x-320)+cam.viewportWidth || c.col().y > (cam.position.y-200)+cam.viewportHeight);
+			 return !(c.col().x < cam.position.x-(cam.viewportWidth/2) || c.col().y < cam.position.y-(cam.viewportHeight/2) || c.col().x > cam.position.x+(cam.viewportWidth/2) || c.col().y > cam.position.y+(cam.viewportHeight/2));
 		 }
 		 
 		 public static Rectangle camera_rec(Camera c) {
-				return new Rectangle(c.position.x-320,c.position.y-200,(c.position.x-320)+c.viewportWidth,(c.position.y-200)+c.viewportHeight);
+				return new Rectangle(c.position.x-(c.viewportWidth/2),c.position.y-(c.viewportHeight/2),c.position.x+(c.viewportWidth/2),c.position.y+(c.viewportHeight/2));
 		}
 	 }
 }
--- core/src/csl/suelo/clases/Mundo.java
+++ core/src/csl/suelo/clases/Mundo.java
@@ -1,5 +1,56 @@
 package csl.suelo.clases;
 
-public class Mundo {
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.utils.Array;
 
+import csl.espacio.clases.Rendereable;
+
+public class Mundo{
+	
+	private Camera cam;
+	private Array<Rendereable> bg;
+	private Array<Enemigo> en;
+	private Array<Objeto> ob;
+	private Array<Wall> wall;
+	private Pj player;
+	
+	public Mundo(Camera cm){
+	cam = cm;
+	bg = new Array<Rendereable>();
+	en = new Array<Enemigo>();
+	ob = new Array<Objeto>();
+	wall = new Array<Wall>();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T extends Pj>T add_player(float x,float y,float w,float h){
+		return (T)(new Pj(x,y,w,h));
+	}
+	
+	public void add_fondo(Rendereable r){
+		bg.add(r);
+	}
+	
+	public void add_enemigo(Enemigo e) {
+		en.add(e);
+	}
+	
+	public void add_Objeto(Objeto o){
+		ob.add(o);
+	}
+	
+	public void add(Wall w){
+		wall.add(w);
+	}
+	
+	
+	public void Actuar(Batch batch,float delta){
+		calculo();
+	}
+	
+	private void calculo(){
+		
+	}
+	
 }
